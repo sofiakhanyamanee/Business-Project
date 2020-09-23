b7939e63dec7b164e43a4b83467d64b1a69c7d49
Sofia Khan Yamanee <sofiakhan.y@hotmail.com>
2020-09-24 00:52:27 +0200

    Add UserContext.Provider

--- src/App.js
+++ src/App.js
@@ -6,44 +6,48 @@ import HomePage from "./pages/HomePage";
 import BaseLayout from "./components/BaseLayout";
 import CustomerDetailPage from "./pages/CustomerDetailPage";
 import { CustomerContext } from "./contexts/CustomerContext";
+import { UserContext } from "./contexts/UserContext";
 
 function App() {
+  const [userInfo, setUserInfo] = useState([]);
   const [customerList, setCustomerList] = useState([]);
 
   return (
     <div>
-      <CustomerContext.Provider value={{ customerList, setCustomerList }}>
-        <Switch>
-          <Route
-            path="/customer/:id"
-            render={(props) => {
-              return (
-                <BaseLayout>
-                  <CustomerDetailPage {...props} />
-                </BaseLayout>
-              );
-            }}
-          ></Route>
+      <UserContext.Provider value={{ userInfo, setUserInfo }}>
+        <CustomerContext.Provider value={{ customerList, setCustomerList }}>
+          <Switch>
+            <Route
+              path="/customer/:id"
+              render={(props) => {
+                return (
+                  <BaseLayout>
+                    <CustomerDetailPage {...props} />
+                  </BaseLayout>
+                );
+              }}
+            ></Route>
 
-          <Route path="/home">
-            <BaseLayout>
-              <HomePage />
-            </BaseLayout>
-          </Route>
+            <Route path="/home">
+              <BaseLayout>
+                <HomePage />
+              </BaseLayout>
+            </Route>
 
-          <Route path="/login">
-            <BaseLayout>
-              <LoginPage />
-            </BaseLayout>
-          </Route>
+            <Route path="/login">
+              <BaseLayout>
+                <LoginPage />
+              </BaseLayout>
+            </Route>
 
-          <Route exact path="/">
-            <BaseLayout>
-              <RegisterPage />
-            </BaseLayout>
-          </Route>
-        </Switch>
-      </CustomerContext.Provider>
+            <Route exact path="/">
+              <BaseLayout>
+                <RegisterPage />
+              </BaseLayout>
+            </Route>
+          </Switch>
+        </CustomerContext.Provider>
+      </UserContext.Provider>
     </div>
   );
 }
