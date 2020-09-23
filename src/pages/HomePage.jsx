8071535952f637ee4addb146878011701ef37b2e
Sofia Khan Yamanee <sofiakhan.y@hotmail.com>
2020-09-24 00:58:01 +0200

    Set userInfo in UserContext

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import CreateCustomer from '../components/CreateCustomer'
 import CustomerList from '../components/CustomerList'
 import { CustomerContext } from '../contexts/CustomerContext'
+import { UserContext } from '../contexts/UserContext'
 import UserKit from '../data/UserKit'
 
 export default function HomePage() {
-
   const {customerList, setCustomerList} = useContext(CustomerContext)
+  const {userInfo, setUserInfo} = useContext(UserContext)
 
   const userKit = new UserKit();
 
@@ -19,8 +20,22 @@ export default function HomePage() {
     })
   }
 
+  function fetchUser(){
+  userKit.getUser()
+  .then(res => res.json())
+  .then(data => {
+    setUserInfo(data)
+  })
+}
+
+useEffect(()=> {
+  fetchUser()
+}, [])
+
   return (
-    <div value={{customerList, setCustomerList}}>
+    <div value={{customerList, setCustomerList, userInfo, setUserInfo}}>     
+    <h2>Användare: {userInfo.firstName}</h2>
+    <h3>{userInfo.email}</h3>
       <CustomerList fetchCustomers={fetchCustomers}/>
       <CreateCustomer fetchCustomers={fetchCustomers}/>
     </div>
