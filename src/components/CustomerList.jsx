c84f4f53f8c55a079e9eb54a1879e16c17459429
Sofia Khan Yamanee <sofiakhan.y@hotmail.com>
2020-09-24 10:36:41 +0200

    Add text if customerList is empty

--- src/components/CustomerList.jsx
+++ src/components/CustomerList.jsx
@@ -1,31 +1,37 @@
-import React, { useContext, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-import { CustomerContext } from '../contexts/CustomerContext'
+import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { CustomerContext } from "../contexts/CustomerContext";
 
 const Wrapper = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-flex-direction: column;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
-export default function CustomerList({fetchCustomers}) {
+export default function CustomerList({ fetchCustomers }) {
+  const { customerList } = useContext(CustomerContext);
 
-  const { customerList } = useContext(CustomerContext)
-
-  useEffect(()=> {
-    fetchCustomers()
-  }, [])
-
+  useEffect(() => {
+    fetchCustomers();
+  }, []);
 
   return (
-    <Wrapper> 
+    <Wrapper>
       <h1>Home</h1>
-          {customerList && customerList.map(customerItem => {
-            const id = customerItem.id
-          return <Link to={`/customer/${id}`} key={id}>{customerItem.name}</Link>
-          })}
+      {customerList.length > 0 ? (
+        customerList.map((customerItem) => {
+          const id = customerItem.id;
+          return (
+            <Link to={`/customer/${id}`} key={id}>
+              {customerItem.name}
+            </Link>
+          );
+        })
+      ) : (
+        <p>noooo cuzzz</p>
+      )}
     </Wrapper>
-  )
+  );
 }
