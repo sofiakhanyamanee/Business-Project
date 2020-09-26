2b8845e80ac0e0dca27e631c4b58fe9dd1679d06
Sofia Khan Yamanee <sofiakhan.y@hotmail.com>
2020-09-26 13:24:03 +0200

    Create activate user component

---
+++ src/components/ActivateUser.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ActivateUser() {
+  return (
+    <div>
+      <h2>Almost there, please check your email to activate your account!</h2>
+    </div>
+  )
+}
