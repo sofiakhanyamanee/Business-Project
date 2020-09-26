514504830cd80b33150c4412d558a9beff4a6314
Sofia Khan Yamanee <sofiakhan.y@hotmail.com>
2020-09-26 13:24:18 +0200

    Create activate user page

---
+++ src/pages/ActivateUserPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ActivateUser from '../components/ActivateUser'
+
+export default function ActivateUserPage() {
+  return (
+    <div>
+      <ActivateUser/>
+    </div>
+  )
+}
