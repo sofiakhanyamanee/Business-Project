f246ae39cba0096fe92ea16db3bc60afb5b6b626
Sofia Khan Yamanee <sofiakhan.y@hotmail.com>
2020-09-27 15:43:35 +0200

    Removed useEffect

--- src/components/CreateCustomer.jsx
+++ src/components/CreateCustomer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import UserKit from "../data/UserKit";
 import styled from 'styled-components'
 import { useForm } from 'react-hook-form'
